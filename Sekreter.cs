f0be397da36fa46140b97e9d440a49d701118987
Egemen Demirkan <egemendemirkan@posta.mu.edu.tr>
2025-12-23 20:55:25 +0300

    Add Sekreter class extending Personel

---
+++ Sekreter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    public class Sekreter : Personel
+    {
+        public Sekreter()
+        {
+            this.Rol = "Sekreter";
+        }
+    }
+}
