f4ff80048b553d1701ab926f5e9d9ead54e7fe41
Egemen Demirkan <egemendemirkan@posta.mu.edu.tr>
2025-12-23 20:54:10 +0300

    Create Hasta.cs

---
+++ Hasta.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    public class Hasta
+    {
+        public string TCKimlikNo { get; set; }
+        public string AdSoyad { get; set; }
+        public string Telefon { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+    }
+}
