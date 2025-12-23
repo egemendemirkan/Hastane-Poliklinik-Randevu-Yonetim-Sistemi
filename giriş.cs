77ae510a143edc1578cf877efc8602d7d48a507c
Egemen Demirkan <egemendemirkan@posta.mu.edu.tr>
2025-12-23 20:56:46 +0300

    Add giriş form with navigation buttons

---
+++ giriş.cs
@@ -0,0 +1,47 @@
+namespace poliklinik_randevu_sistemi
+{
+    public partial class giriş : Form
+    {
+        public giriş()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void giriş_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            giriş2 giris2 = new giriş2();
+            this.Hide();
+            giris2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            giriş2 giris2 = new giriş2();
+            this.Hide();
+            giris2.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            giriş2 giris2 = new giriş2();
+            this.Hide();
+            giris2.Show();
+        }
+    }
+}
