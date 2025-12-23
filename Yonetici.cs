3c378b7cb427bbbe350e90381de630da5385f153
Egemen Demirkan <egemendemirkan@posta.mu.edu.tr>
2025-12-23 20:55:56 +0300

    Create Yonetici.cs

---
+++ Yonetici.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    public class Yonetici : Personel
+    {
+        public Yonetici()
+        {
+            this.Rol = "Yonetici";
+        }
+    }
+}
